9bf87b8384763b7ce411649a5036f4ee3d5beb83
pious <109950629+pious2847@users.noreply.github.com>
2024-06-12 16:56:23 +0000

    Changes to be committed: 	new file:   .env 	modified:   package-lock.json 	modified:   package.json 	new file:   public/livetracking.jpg 	modified:   src/App.css 	modified:   src/components/Home/HomeBody.jsx

--- src/components/Home/HomeBody.jsx
+++ src/components/Home/HomeBody.jsx
@@ -1,33 +1,29 @@
-import { Button } from "@/components/ui/button";
-import ThreeTierPricing from "../pricing/Pricing";
 
 function HomeBody() {
   return (
     <>
-      <div className="flex gap-10 flex-col align-middle relative boldermessage">
-        <h2
-          className="text-7xl flex-1 font-extrabold tracking-tight "
-          style={{ textAlign: "center"  }}
-        >
-          Creative power that goes beyond templates
-        </h2>
-        <p style={{ textAlign: "center" }} className="text-xl">
-          You design, we generate the code — for everything from fully custom{" "}
-          <br /> layouts to complex animations.
-        </p>
-        <div className="align-middle flex justify-center">
-          <Button className="w-44 h-12 neon ">
-            {/* <Link href="/login"></Link> */}
-            Get Started - i’s Free
-          </Button>
+      <div className="container  flex gap-10  w-[95%] p-8 align-middle rounded-lg shadow-lg shadow-black relative boldercontainer">
+        <div className="flex flex-col gap-3   width_100 captioncontianer">
+          <h2
+            className="text-4xl flex-1 font-extrabold tracking-tight"
+            style={{ textAlign: "left" }}
+          >
+            Live Tracking
+          </h2>
+          <p className="text-justify gap-2">
+            FindSafe mobile app allows you to live track your mobile device
+            connected to your user account. With the app’s real-time location
+            tracking feature, you can monitor the whereabouts of your device at
+            all times, ensuring its safety and security. Whether you’ve
+            misplaced your phone or suspect it has been stolen, the live
+            tracking functionality provides you with up-to-date location
+            information, enabling you to take necessary actions promptly. Enjoy
+            peace of mind knowing that you can always keep tabs on your valuable
+            device with FindSafe’s mobile live tracking capabilities.
+          </p>
         </div>
-
-        <div className="flex flex-wrap gap-10 -z-10">
-          <ThreeTierPricing />
-        </div>
-        <br />
-        <br />
-        <br />
+          <img src="livetracking.jpg"  className="shadow-lg shadow-black  rounded-lg object-cover w-[70%] h-[350px]" />
+      
       </div>
     </>
   );
