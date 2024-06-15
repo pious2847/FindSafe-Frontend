4f731b5f0ebaa360ac69422269a846d83e5ae2e9
pious <109950629+pious2847@users.noreply.github.com>
2024-06-15 16:43:14 +0000

    Changes to be committed: 	modified:   src/App.jsx 	modified:   src/main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { ThemeProvider } from "./components/theme-provider";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
 import MainPanel from "./views/Home/home";
 import LoginPage from "./views/auth/login";
 import SignupPage from "./views/auth/signup";
@@ -15,13 +16,13 @@ function App() {
       <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
         <Router>
           <Routes>
+            <Route path="/" element={<MainPanel />} />
             <Route path="/docs" element={<DocumentationPage />} />
             <Route path="/about" element={<AboutPage />} />
             <Route path="/pricing" element={<PricingPage />} />
             <Route path="/dashboard" element={<UserDashboard />} />
             <Route path="/signup" element={<SignupPage />} />
             <Route path="/login" element={<LoginPage />} />
-            <Route path="/" element={<MainPanel />} />
           </Routes>
         </Router>
       </ThemeProvider>
