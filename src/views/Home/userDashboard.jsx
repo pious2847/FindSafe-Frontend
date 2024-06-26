c75eb05535e2ff8c52be80e80b14dc1f639e6e53
pious <109950629+pious2847@users.noreply.github.com>
2024-06-26 21:22:54 +0000

    Changes to be committed: 	modified:   src/components/phone/phoneCard.jsx 	modified:   src/views/Home/userDashboard.jsx

--- src/views/Home/userDashboard.jsx
+++ src/views/Home/userDashboard.jsx
@@ -1,25 +1,8 @@
-import { useState, useEffect } from 'react';
 import GoogleMaps from "@/components/Maps/GoogleMaps";
 import PhoneCards from "@/components/phone/phoneCard";
-import { fetchUserDevices } from "@/services/device";
-import { getUserId } from "@/auth/auth";
 
 const UserDashboard = () => {
-  const [phones, setPhones] = useState([]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const userId = getUserId();
-      const fetchedPhones = await fetchUserDevices(userId);
-      setPhones(fetchedPhones);
-    };
-
-    fetchData();
-  }, []); 
-
-  console.log('Fetched phones:', phones); // Log the fetched data
  
-  
 
   return (
     <>
@@ -28,13 +11,9 @@ const UserDashboard = () => {
           <GoogleMaps />
         </div>
         <div className="flex flex-col gap-4 devicecard shadow-sm shadow-slate-700 rounded-md h-[600px] p-2 overflow-auto">
-          {phones && phones.length > 0 ? (
-            phones.map((phone) => (
-              <PhoneCards key={phone._id} Phone={phone} />
-            ))
-          ) : (
-            <p>No phones found</p>
-          )}
+          
+              <PhoneCards />
+           
         </div>
       </div>
     </>
