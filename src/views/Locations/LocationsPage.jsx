2ac997e292ae8da117b659801f3121a9e20ab241
pious <109950629+pious2847@users.noreply.github.com>
2024-06-29 20:13:20 +0000

    Changes to be committed: 	modified:   src/App.css 	modified:   src/views/Locations/LocationsPage.jsx 	modified:   src/views/settings/settings.jsx

--- src/views/Locations/LocationsPage.jsx
+++ src/views/Locations/LocationsPage.jsx
@@ -3,6 +3,7 @@ import LocationsTable from "@/components/Tables/LocationTable";
 const LocationsDataPage = () => {
     return (  
         <>
+      <h2 className="text-xl font-semibold mb-6">Locations</h2>
         <LocationsTable/>
         </>
     );
--- src/views/settings/settings.jsx
+++ src/views/settings/settings.jsx
@@ -1,8 +1,28 @@
+import { FaPalette } from "react-icons/fa";
+import { ModeToggle } from "@/components/mode-toggle";
+import { CiUser } from "react-icons/ci";
+
 const SettingsPage = () => {
-    return ( 
-        <>
-        </>
-     );
-}
- 
+
+  return (
+    <div className="flex flex-col gap-4 settingspage h-screen">
+      <h2 className="text-xl font-semibold mb-6">Settings</h2>
+      <div className="flex items-center justify-between mb-4">
+        <div className="flex items-center gap-2">
+          <FaPalette className="text-xl" />
+          <span>Dark Mode</span>
+        </div>
+        <ModeToggle />
+      </div>
+      <div className="flex items-center justify-between mb-4">
+        <div className="flex items-center gap-2">
+          <CiUser className="text-xl" />
+          <span>Account</span>
+        </div>
+        <button className="text-blue-500">Manage</button>
+      </div>
+    </div>
+  );
+};
+
 export default SettingsPage;
