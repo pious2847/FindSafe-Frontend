bf9cb166bf5409eaa168b5fb3234714b90999991
pious <109950629+pious2847@users.noreply.github.com>
2024-07-06 16:24:09 +0000

    Changes to be committed: 	renamed:    src/services/websocketUtils.jsx -> src/services/websocketUtils.js

--- src/services/websocketUtils.js
+++ src/services/websocketUtils.js
@@ -1,23 +1,25 @@
 /* eslint-disable no-unused-vars */
 // websocketUtils.js
-import { useEffect, useCallback } from 'react';
-import useWebSocket from 'react-use-websocket';
+import { useEffect, useCallback , useState} from 'react';
+import useWebSocket, {ReadyState } from 'react-use-websocket';
 
 const WEBSOCKET_URL = 'wss://findsafe-backend.onrender.com/666c6365db6047c5931510c0';
 
 export const useWebSocketCommand = () => {
+    const [socketUrl, setSocketUrl] = useState(WEBSOCKET_URL);
   const {
     sendMessage,
     lastMessage,
     readyState,
     getWebSocket
-  } = useWebSocket(WEBSOCKET_URL,{
+  } = useWebSocket(socketUrl,{
     onOpen: () => console.log('WebSocket connection established.'),
     onClose: () => console.log('WebSocket connection closed.'),
     onError: (error) => console.error('WebSocket error:', error),
     shouldReconnect: (closeEvent) => true, // Attempt to reconnect on all close events
   });
 
+  const isConnected = readyState === ReadyState.OPEN;
   useEffect(() => {
     if (lastMessage !== null) {
       // Handle incoming messages here
@@ -36,6 +38,9 @@ export const useWebSocketCommand = () => {
     } else {
       console.error('WebSocket is not connected');
       // You might want to queue the message or retry the connection here
+      if (readyState !== ReadyState.CONNECTING) {
+        setSocketUrl(WEBSOCKET_URL);
+      }
     }
   }, [readyState, sendMessage]);
 
