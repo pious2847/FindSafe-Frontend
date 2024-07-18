a7a6dabdd5d23484fbea97dd9ee6b0c8789f850f
pious <109950629+pious2847@users.noreply.github.com>
2024-07-18 20:54:35 +0000

    Changes to be committed: 	modified:   src/components/Hero/HeroSection.jsx

--- src/components/Hero/HeroSection.jsx
+++ src/components/Hero/HeroSection.jsx
@@ -3,7 +3,7 @@ import { CiSaveDown1 } from "react-icons/ci";
 
 function HeroSection() {
   return (
-    <div className="relative top-[60px] overflow-hidden mainhero ">
+    <div className="relative top-[50px] overflow-hidden mainhero ">
       <div className="max-w-7xl mx-auto">
         <div className="relative  pb-8 sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32">
           <main className="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
