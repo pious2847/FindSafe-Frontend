39e7da1c376c78682baa44bd07fa540ad70ec1fc
Abdul Hafis Mohammed <109950629+pious2847@users.noreply.github.com>
2024-12-22 13:42:41 +0000

    update on google maps

--- src/components/Maps/GoogleMaps.jsx
+++ src/components/Maps/GoogleMaps.jsx
@@ -4,7 +4,7 @@ import { APIProvider, Map, AdvancedMarker, Pin, InfoWindow } from "@vis.gl/react
 const GoogleMaps = () => {
     const position = { lat: 0, lng: 0 };
     return (
-        <APIProvider apiKey={process.env.GOOGLE_MAPS_API_KEY}>
+        <APIProvider apiKey={import.meta.env.GOOGLE_MAPS_API_KEY}>
             <div className="h-[100%] w-[100%]">
                 <Map zoom={50} center={position}>
 
