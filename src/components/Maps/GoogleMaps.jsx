83acaf1697242021dcc18a41585e338f7156822b
Abdul Hafis Mohammed <109950629+pious2847@users.noreply.github.com>
2024-12-22 13:12:22 +0000

    added google map api key

--- src/components/Maps/GoogleMaps.jsx
+++ src/components/Maps/GoogleMaps.jsx
@@ -1,17 +1,17 @@
 import { useState } from "react";
-import { APIProvider,  Map, AdvancedMarker, Pin, InfoWindow} from "@vis.gl/react-google-maps";
+import { APIProvider, Map, AdvancedMarker, Pin, InfoWindow } from "@vis.gl/react-google-maps";
 
 const GoogleMaps = () => {
-    const position = {lat: 0, lng: 0};
-    return ( 
-        <APIProvider apiKey='AIzaSyBU7cuBHlqgsfW_wD2XrDgdQYzmdnm-_CM'>
+    const position = { lat: 0, lng: 0 };
+    return (
+        <APIProvider apiKey={process.env.GOOGLE_MAPS_API_KEY}>
             <div className="h-[100%] w-[100%]">
-                <Map zoom={50}  center={position}>
+                <Map zoom={50} center={position}>
 
                 </Map>
             </div>
         </APIProvider>
-     );
-} 
- 
+    );
+}
+
 export default GoogleMaps;
